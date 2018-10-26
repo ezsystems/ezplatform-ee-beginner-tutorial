242b9de4a9e25300d09f3c46d0cd1a8e266f0b26
DominikaK <dominika.kurek@ez.no>
2018-10-26 11:20:22 +0200

    Update step 4 to 2.3

--- src/AppBundle/DependencyInjection/AppExtension.php
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -6,11 +6,8 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
-use Symfony\Component\Yaml\Yaml;
-use Symfony\Component\Config\Resource\FileResource;
 
-class AppExtension extends Extension implements PrependExtensionInterface
+class AppExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -19,13 +16,5 @@ class AppExtension extends Extension implements PrependExtensionInterface
             new FileLocator(__DIR__ . '/../Resources/config')
         );
         $loader->load('services.yml');
-    }
-
-    public function prepend(ContainerBuilder $container)
-    {
-        $configFile = __DIR__ . '/../Resources/config/blocks.yml';
-        $config = Yaml::parse(file_get_contents($configFile));
-        $container->prependExtensionConfig('ez_systems_landing_page_field_type', $config);
-        $container->addResource(new FileResource($configFile));
     }
 }
